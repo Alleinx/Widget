189657aebeec4a2f00ef2a9ece6ffef4bc1d1deb
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-03-09 18:25:22 +0800

    mv apparent_temp.c -> weather and add data.py

---
+++ weather/Data.py
@@ -0,0 +1,21 @@
+# This program Get Weather data
+# Version1
+# TODO: modify data format and combine with apparent_temp
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+r = requests.get('https://www.tianqiapi.com/api/?version=v1&cityid=101280701')
+r.encoding = 'utf-8'
+
+parsed_json = json.loads(r.text)
+# print(parsed_json)
+
+for i in range(len(parsed_json['data'])):
+    for j in range(len(parsed_json['data'][i]['hours'])):
+        print(parsed_json['data'][i]['hours'][j])
+
+# relative_humidity = parsed_json['data'][0]['humidity']
+
+ 
