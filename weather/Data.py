9db14e2dce111d9ff1fa995dce680590e707f52c
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2019-03-12 13:40:57 +0800

    Data.py modified

--- weather/Data.py
+++ weather/Data.py
@@ -11,10 +11,23 @@ r.encoding = 'utf-8'
 parsed_json = json.loads(r.text)
 # print(parsed_json)
 
+
+relative_humidity = parsed_json['data'][0]['humidity']
+print('relative humidity: ' + str(relative_humidity))
+
 for i in range(len(parsed_json['data'])):
     for j in range(len(parsed_json['data'][i]['hours'])):
         print(parsed_json['data'][i]['hours'][j])
-
-# relative_humidity = parsed_json['data'][0]['humidity']
 # use hash-table to get value
  
+#wind_level_to_speed_per_sec = dict({'<0级': 0.1},
+#                                {'<1级': 0.8},
+#                                {'<2级': 2.45}, 
+#                                {'<3级': 4.4},
+#                                {'<4级': 6.7},
+#                                {'<5级': 9.35},
+#                                {'<6级': 12.35},
+#                                {'<7级': 15.5},
+#                                {'<8级': 18.95},
+#                                {'<9级': 22.6}, 
+#                                {'<10级': 26.3})
