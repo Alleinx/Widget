7c1a1afff99d4fd8cdc5e367cc8d18a267956e8d
ZhongYi Yu <l630003061@mail.uic.edu.hk>
2020-03-03 02:25:47 +0800

    update main.py, view.py

--- Pbank/src/main.py
+++ Pbank/src/main.py
@@ -8,51 +8,78 @@ from view import View
 class Command(object):
     def __init__(self, view: View):
         self.view = view
+        self.current_window_is_project = True
+        self.project_list = [str(i) for i in range(115)]
 
     def perform_operation(self, command: str):
-        if command[:2] == 'dp':
-            # display project
-            try:
-                count = int(command[2:])
-                print('count:', count)
-                print('display project', command)
+        if command == 'l':
+            if self.current_window_is_project:
+                # Display Project.
+                self.view.display_project(self.project_list, list_project=True)
+            else:
+                # Display Bill.
+                print('Display Bill')
+                raise NotImplementedError
 
-            except ValueError:
-                print('Invalid command {}'.format(command))
+        elif command[0] == 'd':
+            if self.current_window_is_project:
+                # Delete Project.
+                try:
+                    count = int(command[1:])
+                    print('count:', count)
+                    print('Delete Project', command)
+                except ValueError:
+                    print('Invalid command {}'.format(command))
+            else:
+                # Delete Bill.
+                print('delete Bill')
+                raise NotImplementedError
 
-            return
-
-        if command[0] == 'd':
-            # delete project
-            try:
-                count = int(command[1:])
-                print('count:', count)
-                print('Delete Project', command)
-            except ValueError:
-                print('Invalid command {}'.format(command))
-
-            return
-
-        if command[:2] == 'cp':
+        elif command == 'cp':
             # create a new project
+            # PROJECT MODE
             print('Create project', command)
-            return
 
-        if command in ['q', 'quit']:
+        elif command == 'n':
+            if self.current_window_is_project:
+                self.view.display_project(self.project_list)
+            else:
+                # Display selected Bill.
+                raise NotImplementedError
+
+        elif command == 'b':
+            if self.current_window_is_project:
+                self.view.display_project(self.project_list, backward=True)
+            else:
+                # Display selected Bill.
+                raise NotImplementedError
+
+        elif command in ['q', 'quit']:
+            # Terminate the program
             sys.exit(0)
 
-        print('Unknow command: {}'.format(command))
+        elif command.isdigit():
+            if self.current_window_is_project:
+                # Display Bills of selected project
+                raise NotImplementedError
+            else:
+                # Display details of selected Bill.
+                raise NotImplementedError
+
+        else:
+            print('Unknow command: {}'.format(command))
 
 
 if __name__ == '__main__':
-    project_handler = model.GeneralProjectFactory()
-
-    test_item = [str(i) for i in range(100)]
+    # project_handler = model.GeneralProjectFactory()
 
     ui = View()
     controller = Command(ui)
     # Main loop:
     while True:
         ui.display_prompt()
-        command = input()
+        try:
+            command = input('>>> ')
+        except KeyboardInterrupt:
+            sys.exit(0)
         controller.perform_operation(command)
--- Pbank/src/view.py
+++ Pbank/src/view.py
@@ -1,22 +1,33 @@
 class View(object):
     def __init__(self):
+        # Used to indicate the start position of displaying project.
         self.project_iter_counter = 0
-        self.bill_iter_counter = 0
-        self.PROMOTE_STR_LEN = 70
-        self.LEFT_PADDING_LEN = 10
 
-    def display_project(self, project_list, max_item=10):
+        # Used to indicate the start position of displaying bill.
+        self.bill_iter_counter = 0
+
+        # Indicate the total amount of characters of promote message.
+        self.PROMOTE_STR_LEN = 70
+
+    def display_project(self, project_list, max_item=10, *,
+                        list_project=False, backward=False):
         '''
         Display format: (project name, project description),
         with max_item amount of projects per time.
         '''
-        flat = max_item
-        for i in range(self.project_iter_counter, len(project_list)):
-            if flat == 0:
-                break
-            print(project_list[i])
-            self.project_iter_counter += 1
-            flat -= 1
+        if not list_project:
+            if not backward:
+                if self.project_iter_counter < (len(project_list) - max_item):
+                    self.project_iter_counter += max_item
+            else:
+                if self.project_iter_counter >= max_item:
+                    self.project_iter_counter -= max_item
+
+        sentry = min(
+                self.project_iter_counter + max_item, len(project_list))
+
+        for i, v in enumerate(range(self.project_iter_counter, sentry)):
+            print('Index [{}]:\t\t{}'.format(i, project_list[v]))
 
     def display_selected_project(self, project_name, max_item=10):
         '''
@@ -27,9 +38,15 @@ class View(object):
 
     def display_prompt(self):
         print(' Welcome to Pbank '.center(self.PROMOTE_STR_LEN, '-'))
-        print(' Pbank, A self use tally book '.center(self.PROMOTE_STR_LEN, '-'))
+        print(
+            ' Pbank, A self use tally book '.center(
+                self.PROMOTE_STR_LEN, '-'))
         print(' Menu '.center(self.PROMOTE_STR_LEN, '-'))
-        print('-----1. dp[count]: Display current projects'.ljust(self.PROMOTE_STR_LEN, ' '))
-        print('-----2. d[index] : Delete [index]th project'.ljust(self.PROMOTE_STR_LEN, ' '))
+        print(
+            '-----1. l: Display current projects'.ljust(self.PROMOTE_STR_LEN, ' '))
+        print(
+            '-----2. d[index] : Delete [index]th project'.ljust(self.PROMOTE_STR_LEN, ' '))
         print('-----3. cp: Create a new project'.ljust(self.PROMOTE_STR_LEN, ' '))
-        print('-----4. q: quit'.ljust(self.PROMOTE_STR_LEN, ' '))
+        print('-----4. n: Next page'.ljust(self.PROMOTE_STR_LEN, ' '))
+        print('-----5. b: Previous page'.ljust(self.PROMOTE_STR_LEN, ' '))
+        print('-----6. q: Quit'.ljust(self.PROMOTE_STR_LEN, ' '))
